c0f7a64edbc581aa80a8b5434f50ceeb2ffbfb9c
kora colasuonno <kora.colasuonno@gmail.com>
2016-12-12 13:43:23 -0800

    adds working crossbar-clicking listener

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-console.log('hi');
+var board = document.querySelector('.board');
+var bars = [];
+
+// via http://stackoverflow.com/questions/37010277/cannot-push-element-to-array-after-using-queryselectorall
+bars.forEach.call(document.querySelectorAll('.bar'), function(e){
+        bars.push(e);
+});
+
+board.addEventListener('click', function(e){
+  if (e.target.classList.contains('h') || e.target.classList.contains('v')){
+    e.target.classList.toggle('clicked-bar');
+  }
+});
