794f77733f262686e4e0873209128648260d28b9
kora colasuonno <kora.colasuonno@gmail.com>
2016-12-13 14:57:33 -0800

    corrects variable inside players object (paired w name) and stores name of (currently only) player 1 in gameboard array

--- main.js
+++ main.js
@@ -1,55 +1,57 @@
 var board = document.querySelector('.board');
 var boxes = document.querySelectorAll('.box');
+var $divs = document.querySelectorAll('.divs');
 
-var hBars = [
-  [1,2],
-  [3,4],
-  [5,6]
-];
-var vBars = [
-  [1,2,3],
-  [4,5,6]
-];
-var boxes = [
-  [1,2],
-  [3,4]
+var gameBoard = [
+   [1, 2, 3, 4, 5 ],
+   [6, 7, 8, 9, 10],
+   [11,12,13,14,15],
+   [16,17,18,19,20],
+   [21,22,23,24,25]
 ];
 
 var players = [
   {
-    name: p1
+    name: null
   },
   {
-    name: p2
+    name: null
   }
 ];
 
-var p1;
-var p2;
 
 var input1 = document.querySelector('.p1');
 var input2 = document.querySelector('.p2');
 
 input1.addEventListener('keyup', e => {
-  console.log('code', e.keyCode);
+  // console.log('code', e.keyCode);
   if (e.keyCode === 13) {
     input2.focus();
-    p1 = e.target.value;
+    players[0].name = e.target.value;
   }
 })
 input2.addEventListener('keyup', e => {
-  console.log('code', e.keyCode);
+  // console.log('code', e.keyCode);
   if (e.keyCode === 13) {
-    p2 = e.target.value;
+    players[1].name = e.target.value;
   }
 })
 
 
-var handleClick = e => {
-  var position = this.dataset;
+var handleClick = function(e) {
+  if ( e.target.classList.contains('bar') ){
+    console.log(this);
+    this.removeEventListener('click', handleClick);
+    e.target.classList.add('clicked-bar');
+    var position = this.dataset;
+    gameBoard[position.row][position.col] = players[0].name;
+
+  }
 }
 
-
+for ( var i = 0; i < $divs.length; i++ ) {
+  $divs[i].addEventListener('click', handleClick);
+}
 
 
 
