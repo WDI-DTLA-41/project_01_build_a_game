0738bd0f2e8f1a1bf4de598058c0c375552db236
Abi Munoz <abimunozr@gmail.com>
2016-12-12 18:22:46 -0800

    switches colors

--- main.js
+++ main.js
@@ -1,18 +1,35 @@
 // query selector
+var tr = document.querySelectorAll('tr');
 var td = document.querySelectorAll('td');
-var color1 = document.querySelector('.color1');
-var color2 = document.querySelector('.color2');
+var color1 = 'color1';
+var color2 = 'color2';
+var colorHolder = color1;
 var button = document.querySelector('button');
+
 
 // add class on click
 function onClick(){
   console.log(this);
-  // add class of color1
-  this.setAttribute('class', 'color1');
+  if (colorHolder == color1) {
+    colorHolder = color2;
+    console.log(this);
+    // adds .color1
+    this.setAttribute('class', 'color1');
+    // prevents second click
+    this.removeEventListener('click', onClick);
+  } else {
+    colorHolder = color1;
+      console.log(this);
+    // adds .color2
+    this.setAttribute('class', 'color2');
+    //prevents second click
+    this.removeEventListener('click', onClick);
+    console.log(colorHolder + "othrer color")
+  }
 }
 
 
-// add event listeners to individual td
+// adds event listeners to individual td
 td.forEach(function(element){
   console.log(element);
   element.addEventListener('click', onClick);
@@ -22,27 +39,19 @@ td.forEach(function(element){
 
 
 
+// create array for td
+tr.forEach(function(element){
+  console.log(element);
+  ////////////////////////////////////////////////////// HOW DO I RETURN AN ARRAY?
 
-// attempted if/else to change classes
-// function toggle(td) {
-//   if (td) {
-//   // add class of color1
-//   this.setAttribute('class', 'color1');
+});
 
-//   // add x inside
-//   var p = document.createElement('p');
-//   p.innerText = "X";
-//   this.appendChild(p);
-//   } else {
-//       // add class of color1
-//   this.setAttribute('class', 'color2');
 
-//   // add x inside
-//   var p = document.createElement('p');
-//   p.innerText = "O";
-//   this.appendChild(p);
-//   }
-// }
+
+// if (document.querySelector('.color1')) {
+//   return true;
+// } else ()
+
 
 
 
