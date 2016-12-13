b6bda4e50a2d451e122e863771194e1fb51dbe82
Abi Munoz <abimunozr@gmail.com>
2016-12-13 11:36:46 -0800

    creates divs and turns board into arrays

--- main.js
+++ main.js
@@ -1,15 +1,33 @@
 // query selector
-var td = document.querySelectorAll('td');
+var square = document.querySelectorAll('.square');
 var color1 = 'color1';
 var color2 = 'color2';
 var colorHolder = color1;
 var button = document.querySelector('button');
 
 
+var board = [
+  [0, 1, 2, 3, 4, 5, 6],
+  [7, 8, 9, 10, 11, 12, 13],
+  [14, 15, 16, 17, 18, 19, 20],
+  [21, 22, 23, 24, 25, 26, 27],
+  [28, 29, 30, 31, 32, 33, 34],
+  [35, 36, 37, 38, 39, 40, 41]
+];
+
+
+
 
 
 // add class on click
-function onClick(){
+function onClick(evt){
+  console.log(this.target, this); //same
+  var position = this.dataset;
+  console.log(board[position.row][position.col]);
+
+
+
+
   console.log(this);
   if (colorHolder == color1) {
     colorHolder = color2;
@@ -30,63 +48,8 @@ function onClick(){
 }
 
 
-// adds event listeners to individual td
-td.forEach(function(element){
+// adds event listeners to individual square
+square.forEach(function(element){
   console.log(element);
   element.addEventListener('click', onClick);
 });
-
-
-
-
-  ////////////////////////////////////////////////////// HOW DO I RETURN AN ARRAY TR?
-
-var column = document.querySelector('.c1');
-for (var i = 0; i < column.lenth; i++) {
-   console.log(i);
-}
-
-
-
-
-
-
-    ///////////////////////////////////////////////////////
-   ///////////////////////////////////////////////////////
-  // code below make shapes bigger by clicking on them //
- ///////////////////////////////////////////////////////
-///////////////////////////////////////////////////////
-
-
-// // query selector
-// var td = document.querySelectorAll('td');
-// var color1 = document.querySelector('.color1');
-// var color2 = document.querySelector('.color2');
-// var button = document.querySelector('.clear');
-
-// // add class on click
-// function onClick(){
-//   console.log(this);
-//   // add class of .color1
-//   this.setAttribute('class', 'color1');
-
-//   // add x inside
-//   var p = document.createElement('p');
-//   p.innerText = "X";
-//   this.appendChild(p);
-// }
-
-
-// // add event listeners to individual td
-// td.forEach(function(element){
-//   console.log(element);
-//   element.addEventListener('click', onClick);
-// });
-
-
-
-
-
-
-
-
