7ee92ac8ac43bc4c5b58fb3a6ffa6fa99e51356a
Lacey Madison <laceyannmadison@gmail.com>
2016-12-13 12:06:56 -0800

    adds functionality for incorrect guesses

--- js/main.js
+++ js/main.js
@@ -3,11 +3,27 @@ var button = document.querySelector('button');
 var word1 = ['C', 'O', 'F', 'F', 'E', 'E'];
 var word2 = ['I', 'N', 'T', 'E', 'R', 'N', 'E', 'T'];
 var word3 = ['D', 'O', 'G']
+var skeleton = [
+document.querySelector('.head'),
+document.querySelector('.body'),
+document.querySelector('.leftarm'),
+document.querySelector('.rightarm'),
+document.querySelector('.leftleg'),
+document.querySelector('.rightleg')
+]
 var playWord;
 var letter;
+var submit = document.querySelector('.submit');
+var j = 0;
 
+function getLetter(){
+  letter = document.querySelector('input').value;
+  letter = letter.toUpperCase();
+  document.querySelector('input').value = '';
+  gameLogic();
+}
 
-function gameLogic(evt) {
+function getWord(evt) {
   var number = Math.random();
   if (number >= .6){
     playWord = word1;
@@ -16,12 +32,22 @@ function gameLogic(evt) {
   } else if (number < .3){
     playWord = word3;
   }
-  letter = prompt('Guess a letter').toUpperCase();
+}
+ function gameLogic(){
+  for (i = 0; i < playWord.length; i++){
   if (playWord.includes(letter)){
     console.log('yes');
+    letter = null;
+    return;
   } else {
     console.log('no');
+    skeleton[j].style.visibility = "visible";
+    j = j + 1;
+    return;
   }
 }
+}
 
-button.addEventListener('click', gameLogic);
+
+button.addEventListener('click', getWord);
+submit.addEventListener('click', getLetter);
