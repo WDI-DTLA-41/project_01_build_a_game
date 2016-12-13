6244527d2ca3f480e24754d2b9a1705dce7de9d2
Kristopher Parrillo <kkparrillo@gmail.com>
2016-12-13 10:56:54 -0800

    a flash of color

--- JS/main.js
+++ JS/main.js
@@ -7,7 +7,7 @@ var yellowBtn = document.querySelector('#yellow');
 var blueBtn = document.querySelector('#blue');
 var startBtn = document.querySelector('#start');
 var counter = document.querySelector('#counter');
-
+var timerID = null;
 
 
 
@@ -29,9 +29,12 @@ var counter = document.querySelector('#counter');
 
 
 // handle start game
-var handleStartGame = function(){
-  greenBtn.style.removeProperty('background-color');
-  greenBtn.classList.toggle('flash-green');
+var handleStartGame = function()  {
+  greenBtn.style.backgroundColor = 'red';
+  timerId = setTimeout(function() {
+  greenBtn.style.backgroundColor = 'green';
+  },250);
+  // greenBtn.classList.toggle('flash-green');
 };
 
 
