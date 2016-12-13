fc09c45cf8b0d2e174b70e5ae62bdc542cd1afd4
Kristopher Parrillo <kkparrillo@gmail.com>
2016-12-13 09:01:40 -0800

    loading vars

--- JS/main.js
+++ JS/main.js
@@ -1,7 +1,42 @@
 console.log('beep boop beep');
 
 // querySelectors for the Buttons
+var greenBtn = document.querySelector('#green');
+var redBtn = document.querySelector('#red');
+var yellowBtn = document.querySelector('#yellow');
+var blueBtn = document.querySelector('#blue');
+var startBtn = document.querySelector('#start');
+var counter = document.querySelector('#counter');
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// handle start game
+var handleStartGame = function(){
+  greenBtn.style.removeProperty('background-color');
+  greenBtn.classList.toggle('flash-green');
+};
+
+
+// add EventListener for startBtn to start game
+startBtn.addEventListener('click',handleStartGame);
 
 
 
