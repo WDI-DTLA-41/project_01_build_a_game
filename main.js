7517923a22d06973d5d37bd963d9b02ee53d488c
kora colasuonno <kora.colasuonno@gmail.com>
2016-12-14 11:56:03 -0800

    still not working but slightly refactored to be slightly more legible

--- main.js
+++ main.js
@@ -24,14 +24,12 @@ var input1 = document.querySelector('.p1');
 var input2 = document.querySelector('.p2');
 
 input1.addEventListener('keyup', e => {
-  // console.log('code', e.keyCode);
   if ( e.keyCode === 13 ) {
     input2.focus();
     players[0].name = e.target.value;
   }
 })
 input2.addEventListener('keyup', e => {
-  // console.log('code', e.keyCode);
   if ( e.keyCode === 13 ) {
     players[1].name = e.target.value;
   }
@@ -53,39 +51,75 @@ for ( var i = 0; i < $divs.length; i++ ) {
   $divs[i].addEventListener('click', handleClick);
 }
 
+//  g a m e  l o g i c  //
 board.addEventListener('click', function(e) {
   // console.log(e.target.dataset);
   // console.log('gameboard ', gameBoard);
   var pos2 = this.dataset;
-  var x = pos2.row;
-  var y = pos2.col;
+  var x    = pos2.row;
+  var y    = pos2.col;
+  vBarBoxToLeft(x,y);
+  vBarBoxToRight(x,y);
+  hBarBoxBelow(x,y);
+  hBarBoxAbove(x,y);
+});
+
+var vBarBoxToRight = function(x,y) {
   for ( var j = 0; j < $divs.length; j++ ) {
+  // for ( var j in this.dataset ) {
     console.log('x: ' + x,'y: ' + y,'j: ' + j);
     // for clicked vertical bar, checks for clicked bars around box to the right
-    if ( gameBoard[x][y] === 'bar' && gameBoard[x+2][y] === 'bar' &&
-          gameBoard[x+1][y+1] === 'bar' && gameBoard[x+1][y-1] === 'bar' ) {
+    if ( gameBoard[x][y]     === 'bar' && gameBoard[x+2][y]   === 'bar' &&
+         gameBoard[x+1][y+1] === 'bar' && gameBoard[x+1][y-1] === 'bar' ) {
       $divs[j+1].classList.add('player1');
     }
+  }
+}
+var vBarBoxToLeft = function(x,y) {
+  for ( var j = 0; j < $divs.length; j++ ) {
     // for clicked vertical bar, checks for clicked bars around box to the left
-    if ( gameBoard[x][y] === 'bar' && gameBoard[x-2][y] === 'bar' &&
-          gameBoard[x-1][y-1] === 'bar' && gameBoard[x-1][y+1] === 'bar' ) {
+    if ( gameBoard[x][y]     === 'bar' && gameBoard[x-2][y]   === 'bar' &&
+         gameBoard[x-1][y-1] === 'bar' && gameBoard[x-1][y+1] === 'bar' ) {
       $divs[j-1].classList.add('player1');
     }
+  }
+}
+var hBarBoxBelow = function(x,y) {
+  for ( var j = 0; j < $divs.length; j++ ) {
     // for clicked horizontal bar, checks for clicked bars around box below
-    if ( gameBoard[x][y] === 'bar' && gameBoard[x+1][y+1] === 'bar' &&
-          gameBoard[x-1][y+1] === 'bar' && gameBoard[x][y+2] === 'bar' ) {
+    if ( gameBoard[x][y]     === 'bar' && gameBoard[x+1][y+1] === 'bar' &&
+         gameBoard[x-1][y+1] === 'bar' && gameBoard[x][y+2]   === 'bar' ) {
       $divs[j+5].classList.add('player1');
     }
+  }
+}
+var hBarBoxAbove = function(x,y) {
+  for ( var j = 0; j < $divs.length; j++ ) {
     // for clicked horizontal bar, checks for clicked bars around box above
-    if ( gameBoard[x][y] === 'bar' && gameBoard[x-1][y-1] === 'bar' &&
-          gameBoard[x+1][y-1] === 'bar' && gameBoard[x][y-2] === 'bar' ) {
+    if ( gameBoard[x][y]     === 'bar' && gameBoard[x-1][y-1] === 'bar' &&
+         gameBoard[x+1][y-1] === 'bar' && gameBoard[x][y-2]   === 'bar' ) {
       $divs[j-5].classList.add('player1');
     }
   }
-});
+};
 
-
-
+        // var p1Score = document.querySelector('.p1-score');
+        // var p1ScoreNum = 0;
+        // var p2Score = document.querySelector('.p2-score');
+        // var p2ScoreNum = 0;
+        // //  s c o r i n g  l o g i c  //
+        // var updateScore = function() {
+        //   for (var k = 0; k < $divs.length; k++) {
+        //     if (gameboard[x][y] === 'box' && $divs[k].classList.contains('player1')){
+        //       p1ScoreNum++
+        //     }
+        //     if (gameboard[x][y] === 'box' && $divs[k].classList.contains('player2')){
+        //       p2ScoreNum++
+        //     }
+        //     p1Score.textContent = p1ScoreNum;
+        //     p2Score.textContent = p2ScoreNum;
+        //   }
+        // }
 
 
 
