2b60d497ce0a5c12ca4ff0c67627798278f6e1d1
mimi schirm <mimischirm@gmail.com>
2016-12-13 16:11:59 -0800

    creates deck

--- main.js
+++ main.js
@@ -1,23 +1,51 @@
 console.log('meow');
 
 
-//using Stack to organize data in sequential order
-function Stack() {
-  //Action Item - create an empty array of cards
+//define card value and suite
+var value = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q','K','A'];
+var suit = ["Spades", "Hearts", "Clubs", "Diamonds"];
 
- this.cards = new Array();
-  this.makeDeck  = stackMakeDeck;
-  this.shuffle   = stackShuffle;
-  this.deal      = stackDeal;
-  this.draw      = stackDraw;
-  this.addCard   = stackAddCard;
-  this.combine   = stackCombine;
-  this.cardCount = stackCardCount;
+var deck = [];
+
+var createDeck = function(){
+  // loops through 2 - A
+  for (var i = 0; i < value.length; i++) {
+    // loops spades - diamonds
+    for (var j = 0; j < suit.length; j++) {
+      var card = {value: value[i], suit: suit[j]}
+      deck.push(card)
+    }
+  }
 }
 
-// creates a full deck of cards in the stack
-function stackMakeDeck() {
-  var ranks = ('A', '2', '3,', '4', '5', '6', '7', '8', '9', '10',
-   'J', 'Q', 'K');
-  var suits =
-}
+
+
+
+
+
+
+
+
+
+
+
+//createDeck will run the for loop, four times, for each suite
+
+//for loop to go from 1 - 13
+//add the object 'card' and add it to deck array
+//use for loop to individual suites - hearts
+// each time you go through the full loop
+
+//need the value and suite
+
+
+
+
+
+
+
+
+
+
+
+
