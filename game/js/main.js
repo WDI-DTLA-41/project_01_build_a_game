2c6a77b66421f3c87d8c9ec9e943d609c8bd0e78
Andrew Maidah <a.maidah@gmail.com>
2016-12-14 15:38:18 -0800

    starts changing functions to object passable

--- game/js/main.js
+++ game/js/main.js
@@ -1,8 +1,9 @@
 console.log('ha');
 
-var $startButton = document.querySelector('[name="start"]');
-var $dealButton = document.querySelector('[name="deal"]');
-var $hitButton = document.querySelector('[name="hit"]');
+var $startButton = document.querySelector('[name="start"]'),
+    $dealButton = document.querySelector('[name="deal"]'),
+    $hitButton = document.querySelector('[name="hit"]'),
+    $stayButton = document.querySelector('[name="stay"]');
 
 var deck = [],
     shuffledDeck= [];
@@ -34,13 +35,13 @@ var players = [
     name: 'player',
     cash: 0,
     hand: [],
-    score: 0
+    score: null
   },
   {
     name: 'dealer',
     cash: 0,
     hand: [],
-    score: 0
+    score: null
   }
 ]
 
@@ -54,29 +55,62 @@ function nextTurn() {
   }
 }
 
-function score() {
-  // for (var i = 0; i < currentPlayer.hand.length; i++){
-    currentPlayer.score = currentPlayer.score + currentPlayer.hand[currentPlayer.hand.length-1].value;
-    console.log(currentPlayer.score);
-  // }
+// function dealScore() { //try to pass in player object??
+//     currentPlayer.score = currentPlayer.score + currentPlayer.hand[currentPlayer.hand.length-1].value;
+//     console.log(capFirstLetter(currentPlayer.name) + ' score:' + ' ' + currentPlayer.score);
+//     if (currentPlayer.score > 21) {
+//       console.log('Bust, better luck next time...')
+//     } //natural blackjack on deal only
+//     if (currentPlayer.score === 21) {
+//       console.log('Blackjack! ' + capFirstLetter(currentPlayer.name) +  ' wins!');
+//       findWinner();
+//     }
+// }
 
+function dealScore(user) { //experiment passing object through
+  user.score = null;
+  for (var x = 0; x < user.hand.length; x++) {
+    user.score = user.score + user.hand[x].value;
+  }
+  console.log(capFirstLetter(user.name) + ' score:' + ' ' + user.score);
+}
+
+function nonDealScore() {
+  currentPlayer.score = currentPlayer.score + currentPlayer.hand[currentPlayer.hand.length-1].value;
+    console.log(capFirstLetter(currentPlayer.name) + ' ' + currentPlayer.score);
+    if (currentPlayer.score > 21) {
+      console.log('Bust, better luck next time...');
+      findWinner();
+      for (var i = 0; i < (players.length); i++) {
+        currentPlayer.score = null;
+        console.log(currentPlayer.score)
+        nextTurn();
+      }
+    }
+}
+
+function capFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
 //GAMEPLAY
 
 function startGame() {
   createDeck();
-  shuffleDeck();
+  console.log('Game started... good luck.')
 }
 
 $startButton.addEventListener('click', startGame);
 
-function dealCards() {
+function dealCards() { //try to pass in a player object??
+  shuffleDeck();
   for (var i = 0; i < (players.length * 2); i++) {
     currentPlayer.hand.push(shuffledDeck.shift());
-    score();
+    dealScore(currentPlayer);
     nextTurn();
   }
+  console.log(capFirstLetter(currentPlayer.name) + '\'s' + ' turn.');
+  $dealButton.removeEventListener('click', dealCards);
 }
 
 $dealButton.addEventListener('click', dealCards);
@@ -84,9 +118,49 @@ $dealButton.addEventListener('click', dealCards);
 function hit() {
   var card = shuffledDeck.shift();
   currentPlayer.hand.push(card);
-  score();
+  nonDealScore();
 }
 
 $hitButton.addEventListener('click', hit);
 
+function stay() {
+  //end player turn
+  $stayButton.removeEventListener('click', stay);
+  nextTurn();
+  console.log(capFirstLetter(currentPlayer.name) + '\'s' + ' turn.');
+  if (currentPlayer && currentPlayer.name === 'dealer') {
+    dealerGamePlay();
+    findWinner();
+  }
+}
+
+$stayButton.addEventListener('click', stay);
+
+function findWinner() {
+  if ( currentPlayer.score === 21 && players[players.length - 1].score != 21 ) {
+    console.log('Blackjack! You win!')
+    $dealButton.addEventListener('click', dealCards);
+    startNextRound();
+    } if ( players[0].score > players[players.length - 1].score ) {
+        console.log('')
+        startNextRound();
+        $dealButton.addEventListener('click', dealCards);
+      }
+}
+
+function startNextRound() { //1 deck logic only
+  shuffledDeck= [];
+}
+
+function startingCash(user) {
+  user.cash = 100;
+}
+
+//Dealer AI
+
+function dealerGamePlay() {
+  while (currentPlayer.score < 17) {
+    hit();
+  }
+}
 
