e85e351bf0bc9a52ecc8c0d7a818cebb33abcb26
james fowler <jamesfowler1000@gmail.com>
2016-12-14 15:29:39 -0800

    adding second row

--- coding/main.js
+++ coding/main.js
@@ -5,9 +5,6 @@ var inputPlayerTwo = document.getElementById('playerTwoInput');
 var playerOneSpan = document.getElementById('playerOne');
 var playerTwoSpan = document.getElementById('playerTwo');
 
-var redPeace = document.getElementById('redPeace');
-var blackPeace = document.getElementById('blackPeace');
-
 var rowOne = document.getElementById('trRow1');
 var rowTwo = document.getElementById('trRow2');
 var rowThree = document.getElementById('trRow3');
@@ -16,14 +13,18 @@ var rowFive = document.getElementById('trRow5');
 var rowSix = document.getElementById('trRow6');
 var rowSeven = document.getElementById('trRow7');
 
-var ColumnOne = document.getElementById('tdColumnOne');
-var ColumnTwo = document.getElementById('tdColumnTwo');
-var ColumnThree = document.getElementById('tdColumnThree');
-var ColumnFour = document.getElementById('tdColumnFour');
-var ColumnFive = document.getElementById('tdColumnFive');
-var ColumnSix = document.getElementById('tdColumnSix');
-var ColumnSeven = document.getElementById('tdColumnSeven');
+var ColumnOne = document.querySelectorAll('.tdColumnOne');
+var ColumnTwo = document.querySelectorAll('.tdColumnTwo');
+var ColumnThree = document.querySelectorAll('.tdColumnThree');
+var ColumnFour = document.querySelectorAll('.tdColumnFour');
+var ColumnFive = document.querySelectorAll('.tdColumnFive');
+var ColumnSix = document.querySelectorAll('.tdColumnSix');
+var ColumnSeven = document.querySelectorAll('.tdColumnSeven');
 
+var redPeace = document.getElementById('redPeace');
+var blackPeace = document.getElementById('blackPeace');
+
+var board = [[],[],[],[],[],[],[]]
 
 inputPlayerOne.addEventListener('keyup', function(event) {
   if(event.keyCode === 13) {
@@ -36,25 +37,59 @@ inputPlayerTwo.addEventListener('keyup', function(event) {
   }
 });
 
+
 var selectedPeace = null;
 
 redPeace.addEventListener('click', function(event) {
   redPeace.classList.add('activeRed');
   blackPeace.classList.remove('activeBlack');
   console.log('red clicked');
-  selectedPeace = redPeace;
-  return selectedPeace;
+  selectedPeace = event.target.style.backgroundColor;
+  console.log(event.target.style.backgroundColor);
+
 });
 
 blackPeace.addEventListener('click', function(event) {
   blackPeace.classList.add('activeBlack');
   redPeace.classList.remove('activeRed');
+  selectedPeace = event.target.style.backgroundColor;
   console.log('black clicked')
-  selectedPeace = blackPeace;
-  return selectedPeace;
+
 });
 
 rowOne.addEventListener('click', function(event) {
   console.log('clicked');
-})
+  for(var i = 6; i >= 0; i--) {
+    if(ColumnOne[i].getAttribute('id') === 'backGroundColorOne'){
+      console.log('i worked');
+      ColumnOne[i].style.backgroundColor = selectedPeace;
+      break;
+    }
+  }
+});
+rowTwo.addEventListener('click', function(event) {
+  console.log('clicked');
+  for(var i = 6; i >= 0; i--) {
+    if(ColumnTwo[i].getAttribute('id') === 'backGroundColorOne'){
+      console.log('i worked');
+      ColumnTwo[i].style.backgroundColor = selectedPeace;
+      break;
+    }
+  }
+});
+rowThree.addEventListener('click', function(event) {
+  console.log('clicked');
+  for(var i = 6; i >= 0; i--) {
+    if(ColumnThree[i].getAttribute('id') === 'backGroundColorOne'){
+      console.log('i worked');
+      ColumnThree[i].style.backgroundColor = selectedPeace;
+      break;
+    }
+  }
+});
+
+
+
+
+
 
