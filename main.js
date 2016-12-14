560bb18fb7ebd7fc3222f326eee135995c7180b6
Abi Munoz <abimunozr@gmail.com>
2016-12-13 16:39:07 -0800

    attempts game logic

--- main.js
+++ main.js
@@ -7,33 +7,50 @@ var button = document.querySelector('button');
 
 
 var board = [
-  [0, 1, 2, 3, 4, 5, 6],
-  [7, 8, 9, 10, 11, 12, 13],
-  [14, 15, 16, 17, 18, 19, 20],
-  [21, 22, 23, 24, 25, 26, 27],
-  [28, 29, 30, 31, 32, 33, 34],
-  [35, 36, 37, 38, 39, 40, 41]
+  [null, null, null, null, null, null],
+  [null, null, null, null, null, null],
+  [null, null, null, null, null, null],
+  [null, null, null, null, null, null],
+  [null, null, null, null, null, null],
+  [null, null, null, null, null, null]
 ];
 
+var isThisOneEmpty = function(row, col) {
+  if (this.position !== color1 || color2){
+    console.log('true');
+    return true;
+  } else {
+    return false;
+  }
+}
 
-// if to check for array
+// var checkTheOneBelow = function(row, col){
+//   if
+
+// }
+// checkTheOneBelow(3, 4) // true
+// checkTheOneBelow(3, 3) // false
 
 
 
 // add class on click
 function onClick(evt){
-  console.log(this.target, this); //same
+  console.log(this.target); //same
   var position = this.dataset;
-  console.log(board[position.row][position.col]);
+  // console.log(board[position.row][position.col]);
+  console.log('position: ', position);
+  console.log('column: ', position.col);
+  console.log('row: ', position.row);
 
 
 
 
-  console.log(this);
+  console.log('add color to: ', this);
   if (colorHolder == color1) {
     colorHolder = color2;
     // console.log(this);
     // adds .color1
+
     this.setAttribute('class', 'color1');
     // prevents second click
     this.removeEventListener('click', onClick);
