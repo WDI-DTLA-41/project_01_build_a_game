b2d168d43d629088e44fbe9c3251ac21946773a8
kora colasuonno <kora.colasuonno@gmail.com>
2016-12-15 15:47:11 -0800

    replaced confusing data-set crap with OG hard-coded logic

--- main.js
+++ main.js
@@ -3,17 +3,9 @@ var boxes = document.querySelectorAll('.box');
 var input1 = document.querySelector('.p1');
 var input2 = document.querySelector('.p2');
 var $divs = document.querySelectorAll('.divs');
-var x = null;
-var y = null;
 
-var gameBoard = [
-   [0, 1, 2, 3, 4],
-   [0, 1, 2, 3, 4],
-   [0, 1, 2, 3, 4],
-   [0, 1, 2, 3, 4],
-   [0, 1, 2, 3, 4]
-];
-
+var bars = [];
+var boxes = [];
 
 
 var players = [
@@ -40,171 +32,40 @@ input2.addEventListener('keyup', e => {
 })
 
 
-var handleClick = function(e) {
-  if ( e.target.classList.contains('bar') ){
-    // console.log(this);
-    this.removeEventListener('click', handleClick);
-    e.target.classList.add('clicked-bar');
-    var position = this.dataset;
-    x = position.row;
-    y = position.col;
-    x = parseInt(x);
-    y = parseInt(y);
-    gameBoard[y][x] = 'bar';
-    checkPerimeter(e);
-  }
-}
-
-for ( var i = 0; i < $divs.length; i++ ) {
-  $divs[i].addEventListener('click', handleClick);
-}
-
-
-//  g a m e  l o g i c  //
-var checkPerimeter = (e) => {
-  // console.log(e.target);
-  // console.log('x: ' + x,'y: ' + y);
-  // console.log('gameBoard[y][x]: ' + gameBoard[y][x]);
-  // console.log('gameBoard[y + 2][x]: ' + gameBoard[y+2][x]);
-  // console.log('gameBoard[y + 1][x + 1]: ' + gameBoard[y+1][x+1]);
-  // console.log('gameBoard[y + 1][x - 1]: ' + gameBoard[y+1][x-1]);
-  // for ( var j = 0; j < $divs.length; j++ ) {
-          // for ( var j in this.dataset ) {
-          // for clicked vertical bar, checks for clicked bars around box to the right
-      if ( gameBoard[y][x]     === 'bar' &&   //vertical bar at click
-           gameBoard[y+2][x]   === 'bar' &&   //vertical bar opposite of click on right
-           gameBoard[y+1][x+1] === 'bar' &&   //horizontal bar below, right
-           gameBoard[y+1][x-1] === 'bar' ) {  //horizontal bar above, right
-        // console.log('j: ' + j);
-        console.log('box!');
-        $divs[x+5].classList.add('player1');
-      }
-  // }
-}
-  // console.log(e.target.dataset);
-  // console.log('gameboard ', gameBoard);
-
-
-
-
-
-
-
-
-
-
-
-
-
-// //  g a m e  l o g i c  //
-
-// var vBarBoxToLeft = function(x,y) {
-//   for ( var j = 0; j < $divs.length; j++ ) {
-//     // for clicked vertical bar, checks for clicked bars around box to the left
-//     if ( gameBoard[x][y]     === 'bar' && gameBoard[x-2][y]   === 'bar' &&
-//          gameBoard[x-1][y-1] === 'bar' && gameBoard[x-1][y+1] === 'bar' ) {
-//       $divs[j-1].classList.add('player1');
-//     }
-//   }
-// }
-// var hBarBoxBelow = function(x,y) {
-//   for ( var j = 0; j < $divs.length; j++ ) {
-//     // for clicked horizontal bar, checks for clicked bars around box below
-//     if ( gameBoard[x][y]     === 'bar' && gameBoard[x+1][y+1] === 'bar' &&
-//          gameBoard[x-1][y+1] === 'bar' && gameBoard[x][y+2]   === 'bar' ) {
-//       $divs[j+5].classList.add('player1');
-//     }
-//   }
-// }
-// var hBarBoxAbove = function(x,y) {
-//   for ( var j = 0; j < $divs.length; j++ ) {
-//     // for clicked horizontal bar, checks for clicked bars around box above
-//     if ( gameBoard[x][y]     === 'bar' && gameBoard[x-1][y-1] === 'bar' &&
-//          gameBoard[x+1][y-1] === 'bar' && gameBoard[x][y-2]   === 'bar' ) {
-//       $divs[j-5].classList.add('player1');
-//     }
-//   }
-// };
-
-        // var p1Score = document.querySelector('.p1-score');
-        // var p1ScoreNum = 0;
-        // var p2Score = document.querySelector('.p2-score');
-        // var p2ScoreNum = 0;
-        // //  s c o r i n g  l o g i c  //
-        // var updateScore = function() {
-        //   for (var k = 0; k < $divs.length; k++) {
-        //     if (gameboard[x][y] === 'box' && $divs[k].classList.contains('player1')){
-        //       p1ScoreNum++
-        //     }
-        //     if (gameboard[x][y] === 'box' && $divs[k].classList.contains('player2')){
-        //       p2ScoreNum++
-        //     }
-        //     p1Score.textContent = p1ScoreNum;
-        //     p2Score.textContent = p2ScoreNum;
-        //   }
-        // }
-
-
-
-
-
-
-
-// var box1 = {
-//   top: null,
-//   bottom: null,
-//   left: null,
-//   right: null,
-//   owner: null
-// }
-// var box2 = {
-//   top: null,
-//   bottom: null,
-//   left: null,
-//   right: null,
-//   owner: null
-// }
-// var box3 = {
-//   top: null,
-//   bottom: null,
-//   left: null,
-//   right: null,
-//   owner: null
-// }
-// var box4 = {
-//   top: null,
-//   bottom: null,
-//   left: null,
-//   right: null,
-//   owner: null
-// }
-
-
-
-
-// var gameBoard = [
-//    [1, 2, 3, 4, 5 ],
-//    [6, 7, 8, 9, 10],
-//    [11,12,13,14,15],
-//    [16,17,18,19,20],
-//    [21,22,23,24,25]
-// ];
-
-
-
-
-
-
-
-
-
 
 // via http://stackoverflow.com/questions/37010277/cannot-push-element-to-array-after-using-queryselectorall
-// boxes.forEach.call(document.querySelectorAll('.box'), function(e){
-//         boxes.push(e);
-// });
+bars.forEach.call(document.querySelectorAll('.bar'), function(e){
+        bars.push(e);
+});
+boxes.forEach.call(document.querySelectorAll('.box'), function(e){
+        boxes.push(e);
+});
 
-// var bars = [];
-// bars.forEach.call(document.querySelectorAll('.bar'), function(e){
-//         bars.push(e);
-// });
+board.addEventListener('click', function(e){
+  if (e.target.classList.contains('h') || e.target.classList.contains('v')){
+    e.target.classList.add('clicked-bar');
+  }
+});
+
+board.addEventListener('click', function(e){
+  if ( (e.target.classList.contains('h') || e.target.classList.contains('v') ) &&
+        bars[0].classList.contains('clicked-bar') && bars[2].classList.contains('clicked-bar') &&
+        bars[3].classList.contains('clicked-bar') && bars[5].classList.contains('clicked-bar') ) {
+          boxes[0].classList.add('player1');
+  };
+  if ( (e.target.classList.contains('h') || e.target.classList.contains('v') ) &&
+        bars[1].classList.contains('clicked-bar') && bars[3].classList.contains('clicked-bar') &&
+        bars[4].classList.contains('clicked-bar') && bars[6].classList.contains('clicked-bar') ) {
+          boxes[1].classList.add('player1');
+  };
+  if ( (e.target.classList.contains('h') || e.target.classList.contains('v') ) &&
+        bars[5].classList.contains('clicked-bar') && bars[7].classList.contains('clicked-bar') &&
+        bars[8].classList.contains('clicked-bar') && bars[10].classList.contains('clicked-bar') ) {
+          boxes[2].classList.add('player1');
+  };
+  if ( (e.target.classList.contains('h') || e.target.classList.contains('v') ) &&
+        bars[6].classList.contains('clicked-bar') && bars[8].classList.contains('clicked-bar') &&
+        bars[9].classList.contains('clicked-bar') && bars[11].classList.contains('clicked-bar') ) {
+          boxes[3].classList.add('player1');
+  };
+});
