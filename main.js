bfcd36df72acf6507f01fd448b7b5af0d069e016
brad morgan <bkmorgan3@gmail.com>
2016-12-15 15:51:40 -0800

    added Divs on deal, cleaned

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
 // console.log('hi');
 
-
-
 var suits = ["Club", "Heart", "Diamond", "Spade"];
 var values = [2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King", "Ace"];
 
@@ -10,12 +8,18 @@ var deck = [];
 var playerOne = [];
 var dealer = [];
 
+var playerScore = 0;
+
+var newSpan = document.querySelector('#playerscore');
+  newSpan.textContent = playerScore;
+
+
+
 // make deck
-// TODO rename to makeDeck
-var makeCard = function() {
+var makeDeck = function() {
   for(var i = 0; i < suits.length; i++) {
    var suit = suits[i];
-    for( var j = 0; j < values.length; j++) {
+    for(var j = 0; j < values.length; j++) {
       var val = values[j]
 
       var card = {
@@ -26,7 +30,7 @@ var makeCard = function() {
     }
   }
 }
-makeCard();
+makeDeck();
 
 
 //shuffle deck
@@ -45,26 +49,22 @@ var deal = function(){
   var card = deck.pop()
   dealer.push(card);
   var playerScore = getValues(playerOne);
-  console.log(playerScore);
+  console.log("player score is " + playerScore);
   var dealerScore = getValues(dealer);
-  console.log(dealerScore)
-
+  console.log("dealer score is " + dealerScore)
+ // if over 21 game over
     if(playerScore === 21) {
       console.log("Blackjack, Wou Win!");
     }
     if(dealerScore === 21){
       console.log("Dealer has 21.");
     }
-
-
-  // if over 21 game over
   // else get another card
 }
-
-
-
-
-
+var hit = function () {
+    var card = deck.pop()
+    playerOne.push(card);
+}
 // get dealer score
 var getValues = function(cards) {
   var sum = 0;
@@ -85,20 +85,45 @@ var getValues = function(cards) {
     sum += val
   }
   return sum;
-  console.log("sum = " + sum);
+  console.log('working');
 }
 
-var hit = function () {
-    var card = deck.pop()
-    playerOne.push(card);
-}
+// conditions for the win
+// how to set dealerScore
+// if (playerScore > dealerScore) {
+//   console.log("You Win!");
+// } else if(dealerScore > playerScore) {
+//   console.log('You lose.');
+// } else if(playerScore === dealerScore) {
+//   console.log("Push.");
+// } else {
+//   console.log("No one planned for this");
+// }
+
+
+
+var stayButton = document.querySelector('#stay');
+stayButton.addEventListener('click', function (){
+  //  Dealer plays.
+  console.log('dealers turn');
+});
 
 
 var dealButton = document.querySelector('#deal');
 dealButton.addEventListener('click', function() {
+// newSpan.textContent = getValues(playerOne);
+
   deal();
-  // console.log('deal');
-  var playerScore = getValues(playerOne);
+  console.log('deal');
+  var newDiv = document.createElement('div');
+  newDiv.classList.add('card');
+  var newDiv = document.createElement('div');
+  newDiv.classList.add('card');
+
+  document.body.appendChild(newDiv);
+  document.body.appendChild(newDiv);
+
+   playerScore = getValues(playerOne);
     if(playerScore === 21) {
       console.log("You Got 21!");
     }
@@ -107,10 +132,14 @@ dealButton.addEventListener('click', function() {
 var hitButton = document.querySelector('.hit');
 hitButton.addEventListener('click', function(){
     hit();
+    var newDiv = document.createElement('div');
+  newDiv.classList.add('card');
+  document.body.appendChild(newDiv);
     console.log('hit');
-    var playerScore = getValues(playerOne);
+    playerScore = getValues(playerOne);
     console.log(playerScore);
     if(playerScore > 21) {
       console.log("You Busted");
+
     }
 });
