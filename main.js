f65ceb1fdacd0e4a8cada53dadbcac95055d2b08
Abi Munoz <abimunozr@gmail.com>
2016-12-14 18:29:49 -0800

    rearranged functions, still works

--- main.js
+++ main.js
@@ -4,18 +4,24 @@ var color2 = 'color2';
 var colorHolder = color1;
 
 var square = document.querySelectorAll('.square');
-var button = document.querySelector('button');
+var button = document.querySelector('#clear');
 
 
 var board = [
-  [null, null, null, null, null, null],
-  [null, null, null, null, null, null],
-  [null, null, null, null, null, null],
-  [null, null, null, null, null, null],
-  [null, null, null, null, null, null],
-  [null, null, null, null, null, null]
+  [null, null, null, null, null, null, null],
+  [null, null, null, null, null, null, null],
+  [null, null, null, null, null, null, null],
+  [null, null, null, null, null, null, null],
+  [null, null, null, null, null, null, null],
+  [null, null, null, null, null, null, null]
 ];
 
+var isThisOneEmpty = function(position) {
+  console.log("this is empty");
+  var row = parseInt(position.row);
+  var col = parseInt(position.col);
+  return board[row][col] === null;
+}
 
 var checkTheOneBelow = function(position){
   var row = parseInt(position.row) + 1;
@@ -24,18 +30,10 @@ var checkTheOneBelow = function(position){
     console.log("row 5 you can move")
     return true;
   } else {
-
     return board[row][col] !== null;
   }
 }
 
-
-var isThisOneEmpty = function(position) {
-  console.log("this is empty");
-  var row = parseInt(position.row);
-  var col = parseInt(position.col);
-  return board[row][col] === null;
-}
 
 // add class on click
 function onClick(evt){
@@ -56,7 +54,6 @@ function onClick(evt){
       colorHolder = color2;
       // console.log(this);
       // adds .color1
-
       this.setAttribute('class', 'color1');
       // prevents second click
     } else {
@@ -77,3 +74,11 @@ square.forEach(function(element){
   // console.log(element);
   element.addEventListener('click', onClick);
 });
+
+// clear divs with button click
+var clear = function(){
+ // square.classList.remove("color1");
+ console.log('click')
+}
+
+button.addEventListener('click', clear);
