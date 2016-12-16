50fd78bea7889de98ec333e32b50bd75f346c837
Abi Munoz <abimunozr@gmail.com>
2016-12-15 17:07:14 -0800

    working on winning logic

--- main.js
+++ main.js
@@ -41,43 +41,25 @@ var checkTheOneBelow = function(position){
 
 
 
+
 var winColor1 = [ ];
 var winColor2= [ ];
-var object = {};
-
 
 var win = function (position) {
   console.log(position.className);
   console.log("the other here i am 2");
   var row = parseInt(position.row);
   var col = parseInt(position.col);
-  if (position.className === color1 && board[position.row][parseInt(position.col)-1] === color1){
+  // BOTTOM
+if(board[position.col][position.row].className('color1') === board[position.col][position.row + 1].className('color1')){
     winColor1.push(position);
     console.log(winColor1);
     console.log('position: ', position);
     console.log('class: ', position.className);
+    console.log(winColor1);
   }
-
-  console.log(winColor1);
-
-
-  // if (this.position.className === color1 && (this.position.col - 1, this.position.row) === color1){
-  //   winColor1.push();
-  //   console.log('position: ', position);
-  // }
-  // if (this.position.className === color1 && (this.position.row) -1 === color1){
-  //   winColor1.push();
-  //   console.log('position: ', position);
-  // }
-  // if (this.position.className === color1 && (this.position.col + 1, this.position.col-1) === color1){
-  //   winColor1.push();
-  //   console.log('position: ', position);
-  // }
-  // if (this.position.className === color1 && (this.position.col) + 1 === color1){
-  //   winColor1.push();
-  //   console.log('position: ', position);
-  // }
 }
+
 
 
 
--- win.js
+++ win.js
@@ -1,32 +1,58 @@
+var winColor1 = [ ];
+var winColor2= [ ];
+
 var win = function (position) {
   console.log(position.className);
   console.log("the other here i am 2");
   var row = parseInt(position.row);
   var col = parseInt(position.col);
-  if (position.className === color1 && board[position.row][parseInt(position.col)-1] === color1){
-    winColor1.push(position);
+  // BOTTOM
+if(board[position.col][position.row].getAttribute('color1') === board[position.col][position.row + 1].getAttribute('color1')){
+    winColor1.push(board[position.col][position.row]);
     console.log(winColor1);
     console.log('position: ', position);
     console.log('class: ', position.className);
+    console.log(winColor1);
   }
 
   console.log(winColor1);
 
 
-  // if (this.position.className === color1 && (this.position.col - 1, this.position.row) === color1){
-  //   winColor1.push();
-  //   console.log('position: ', position);
-  // }
-  // if (this.position.className === color1 && (this.position.row) -1 === color1){
-  //   winColor1.push();
-  //   console.log('position: ', position);
-  // }
-  // if (this.position.className === color1 && (this.position.col + 1, this.position.col-1) === color1){
-  //   winColor1.push();
-  //   console.log('position: ', position);
-  // }
-  // if (this.position.className === color1 && (this.position.col) + 1 === color1){
-  //   winColor1.push();
-  //   console.log('position: ', position);
-  // }
-}
+// CHECK AROUND EVERY CLICK AND STORE INTO VARIABLE
+// TOP
+if(board[position.col][position.row].getAttribute('color1') === board[position.col][position.row - 1].getAttribute('color1'))
+
+// TOP RIGHT
+if(board[position.col][position.row].getAttribute('color1') === board[position.col + 1][position.row - 1].getAttribute('color1'))
+
+//RIGHT
+if(board[position.col][position.row].getAttribute('color1') === board[position.col + 1][position.row].getAttribute('color1'))
+
+// BOTTOM RIGHT
+if(board[position.col][position.row].getAttribute('color1') === board[position.col + 1][position.row + 1].getAttribute('color1'))
+
+// BOTTOM
+if(board[position.col][position.row].getAttribute('color1') === board[position.col][position.row + 1].getAttribute('color1'))
+
+// BOTTOM LEFT
+if(board[position.col][position.row].getAttribute('color1') === board[position.col - 1][position.row + 1].getAttribute('color1'))
+
+// LEFT
+if(board[position.col][position.row].getAttribute('color1') === board[position.col - 1][position.row].getAttribute('color1'))
+
+// TOP LEFT
+if(board[position.col][position.row].getAttribute('color1') === board[position.col - 1][position.row - 1].getAttribute('color1'))
+
+
+
+TOP WIN
+if(board[position.col][position.row].getAttribute('color1') === board[position.col][position.row - 1].getAttribute('color1') &&
+  (board[position.col][position.row].getAttribute('color1') === board[position.col][position.row - 2].getAttribute('color1') &&
+    (board[position.col][position.row].getAttribute('color1') === board[position.col][position.row - 3].getAttribute('color1')) {
+        console.log('win');
+      }
+
+
+board syntax
+getting class color1 || Color2
+console.log('win')
