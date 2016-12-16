56961b032d458190ff4ad2866164169ee76c441b
mimi schirm <mimischirm@gmail.com>
2016-12-15 20:07:49 -0800

    almost completes mvp

--- main.js
+++ main.js
@@ -4,14 +4,16 @@ var playerHand = [];
 var playerBoard = [];
 var dealerHand = [];
 var dealerBoard = [];
+var tempPlayCard = null;
+var tempDealCard = null;
 
 //deck
-// var deck = [
-//             2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14,
-//             2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14,
-//             2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14,
-//             2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14
-//             ];
+var deck = [
+            2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14,
+            2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14,
+            2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14,
+            2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14
+            ];
 
 
 //console.log(deck);
@@ -57,37 +59,69 @@ var battle = function(){
   dealerBoard = dealerHand.shift();
   console.log('playerBoard: ', playerBoard);
   console.log('dealerBoard: ', dealerBoard);
+compare();
 }
-//battle()
+
 // console.log(battle);
 
 
 //compare cards
 var compare = function(){
-  if (playerBoard > dealerBoard) {
+    if (playerBoard === dealerBoard) {
+    console.log('War!');
+    war();
+    compareWar();
+  } else if (playerBoard > dealerBoard) {
     console.log('player won this hand');
     playerHand.push(playerBoard, dealerBoard);
-  } else if (dealerBoard > playerBoard) {
+  } else {
     console.log('dealer won this hand');
     dealerHand.push(dealerBoard, playerBoard);
-  } else {
-    // war
-    war()
   }
-}
+};
+
 
 //war
-var war = function(){
-  if (playerBoard === dealerBoard){
-    console.log('War!');
+ function war(){
+    //playerBoard + dealerBoard is getting thrown away in the trash
+    //will need to add those cards to the other 4 cards
+    tempPlayCard = playerBoard;
+    tempDealCard = dealerBoard;
     playerBoard = playerHand.splice(0,4);
-  //dealerHand.splice(0,3);
+    console.log("playerHand is " + playerBoard);
+    dealerBoard = dealerHand.splice(0,4);
+    console.log("dealerHand is " + dealerBoard);
+
+  }
+
+
+//compare war
+function compareWar(){
+    if (playerBoard[3] > dealerBoard[3]){
+    console.log('player won this war');
+    playerHand.push.apply(playerBoard, dealerBoard);
+    playerHand.push(tempPlayCard, tempDealCard);
+  } else if (dealerBoard[3] > playerBoard[3]) {
+    console.log('dealer won this war');
+    dealerHand.push.apply(dealerBoard, playerBoard);
+    dealerHand.push(tempDealCard, tempPlayCard);
+   }
+  }
+
+var winner = function(){
+  if (playerBoard.length === 52) {
+   console.log('player wins the game');
+  } else if (dealerBoard.length === 52) {
+    console.log('dealer wins the game');
+  } else {
+    console.log('No Winner');
   }
 }
 
-  //using splice to take four cards
-  //compare the last element of the array
+//ACTION: function to add wins/loss of both player and dealer
 
+//Notes: see below
+//for loop with .shift ---> insteading of splicing?
 
 // if dealerBoard or playerBoard === 'undefined', game should lose
 
